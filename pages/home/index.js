ebc2325582927e05b9b5edfe30968a75c5a49683
Yang Bo <yang7@michaels.coom>
2024-02-05 15:46:09 +0800

    Initial Commit

---
+++ pages/home/index.js
@@ -0,0 +1,58 @@
+// pages/home/index.js
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+
+  /**
+   * todo 这里只是模拟登录的授权，后续改成真正的授权 要获取用户的手机号
+   */
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail;
+    wx.redirectTo({
+      url: "/pages/qa/index",
+    });
+  },
+});
